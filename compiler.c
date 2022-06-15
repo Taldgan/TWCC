4d0e13b2905b0521ccaa80ea17838603a997c961
Taldgan <bananaphone993@live.com>
2022-06-15 13:39:18 -0700

    basic parsing implementation & AST data types
    Implemented
    - parseStatement
    - parseExpression
    - parseProgram
    - parseFunction

--- compiler.c
+++ compiler.c
@@ -4,9 +4,45 @@
 #include <regex.h>
 #include <ctype.h>
 
+/**
+ * TODO
+ * remember the use of union - which would allow me to assign multiple data types to one AST node
+ * Define astnode data type, for use in parse tree
+ * Maybe make 'popped' token list? for easy free's?
+ **/
+
+/**
+ * Custom data types
+ **/
 #define LEN_PATH 4097
 #define NUM_KEYWORDS 9
 
+//Abstract Syntax Tree data types
+typedef enum AST_TYPE {PROGRAM, FUNCTION, STATEMENT, EXPRESSION, FUNCNAME} AST_TYPE;
+
+/**
+ * For use in AST node data type
+ *
+ **/
+typedef union fields {
+    int intVal;
+    char *strVal;
+    struct children{
+      struct astnode_t *left;
+      struct astnode_t *right;
+    } children;
+} fields;
+
+/**
+ * AST Node data type.
+ * Depending on type of node, union data type is set to either have children (like with operators), or a value
+ **/
+typedef struct astnode_t {
+  AST_TYPE nodeType;
+  fields fields;
+} astnode_t;
+
+//Token-related data types
 typedef enum TOKEN_TYPE {OPEN_BRACE, CLOSED_BRACE, OPEN_PAREN, CLOSED_PAREN, SEMICOLON, INT_KEYW, RET_KEYW, INT_LITERAL, IDENTIFIER} TOKEN_TYPE;
 
 //Tokenlist node, contains token data and pointer to next token (if available)
@@ -62,7 +98,7 @@ void initRegexp(){
     fprintf(stderr, "Failed to init 1 or more regular expressions.\n");
     exit(1);
   }
-  //Assign 'single' keywords
+  //Assign keywords to regex_t types
   keywords[0] = openBrace;
   keywords[1] = closeBrace;
   keywords[2] = openParen;
@@ -232,7 +268,7 @@ tokenlist_t *lex(){
   char *line = 0;
   int lineNum = 0;
 
-  printf("-- lexing %s --\n\n", sourcePath);
+  printf("── lexing %s ──\n\n", sourcePath);
   line = strtok(fileBuf, "\n");
   //Parse line for tokens
   while(line){
@@ -257,13 +293,15 @@ tokenlist_t *lex(){
             tokType = (TOKEN_TYPE) i;
           }
         }
+        //Need to add else for invalid token types...
       }
       printf("\tToken found: ");
       printSubstr(line, minStart, minEnd);
       puts("");
       token_t *newToken = createToken(strndup(&line[minStart], minEnd-minStart), tokType);
       appendToken(tokens, newToken);
-      printf("\t\tAdded token: %s\n", newToken->value);
+      numTokens++;
+      //printf("\t\tAdded token: %s\n", newToken->value);
       //Update line position (and line length) to remove identified token
       line += minEnd;
       lineLen -= minEnd;
@@ -321,9 +359,146 @@ void freeTokens(tokenlist_t *tokens){
   free(tokens);
 }
 
+astnode_t *parseExpression(tokenlist_t *tokens){
+  token_t *currToken = NULL;
+  astnode_t *exprNode = NULL;
+  currToken = popToken(tokens);
+  if(currToken == NULL || currToken->type != INT_LITERAL){
+    fprintf(stderr, "Int literal did not follow return statement, invalid format.\n");
+    exit(1);
+  }
+  exprNode = (astnode_t *) malloc(sizeof(astnode_t)*1);
+  exprNode->nodeType = EXPRESSION;
+  if(exprNode == NULL){
+    fprintf(stderr, "Failed to allocate space for expression node.\n");
+    exit(1);
+  }
+  exprNode->fields.intVal = atoi(currToken->value);
+  return exprNode;
+}
+
+astnode_t *parseStatement(tokenlist_t *tokens){
+  token_t *currToken = NULL;
+  astnode_t *statementNode = NULL;
+  if(tokens == NULL){
+    fprintf(stderr, "Cannot parse statement, null token list.\n");
+    exit(1);
+  }
+  currToken = popToken(tokens);
+  if(currToken == NULL || currToken->type != RET_KEYW){
+    fprintf(stderr, "Statement did not begin with return, invalid format.\n");
+    exit(1);
+  }
+  statementNode = (astnode_t *) malloc(sizeof(astnode_t)*1);
+  statementNode->nodeType = STATEMENT;
+  statementNode->fields.children.left = parseExpression(tokens);
+  currToken = popToken(tokens);
+  if(currToken == NULL || currToken->type != SEMICOLON){
+    fprintf(stderr, "Statement did not end with semicolon, invalid format.\n");
+    exit(1);
+  }
+  return statementNode;
+}
+
+astnode_t *parseFunction(tokenlist_t *tokens){
+  token_t *currToken = NULL;
+  currToken = popToken(tokens);
+  astnode_t *funcNode = NULL;
+  char *funcName = NULL;
+  if(currToken == NULL || currToken->type != INT_KEYW){
+    fprintf(stderr, "Function did not begin with int keyword, invalid format.\n");
+    exit(1);
+  }
+  currToken = popToken(tokens);
+  if(currToken == NULL || currToken->type != IDENTIFIER){
+    fprintf(stderr, "Identifier did not follow int keyword, invalid format.\n");
+    exit(1);
+  }
+  funcName = currToken->value;
+  funcNode = (astnode_t *) malloc(sizeof(astnode_t)*1);
+  funcNode->nodeType = FUNCTION;
+  funcNode->fields.children.left = (astnode_t *) malloc(sizeof(astnode_t)*1);
+  funcNode->fields.children.left->nodeType = FUNCNAME;
+  if(funcNode->fields.children.left == NULL){
+    fprintf(stderr, "Failed to allocate space for function name node.\n");
+    exit(1);
+  }
+  //Function left child node will contain value of function's name, right func body
+  funcNode->fields.children.left->fields.strVal = funcName;
+  currToken = popToken(tokens);
+  if(currToken == NULL || currToken->type != OPEN_PAREN){
+    fprintf(stderr, "Open parenthese did not follow identifier, invalid format.\n");
+    exit(1);
+  }
+  currToken = popToken(tokens);
+  if(currToken == NULL || currToken->type != CLOSED_PAREN){
+    fprintf(stderr, "Closed parenthese did not follow open parenthese, invalid format.\n");
+    exit(1);
+  }
+  currToken = popToken(tokens);
+  if(currToken == NULL || currToken->type != OPEN_BRACE){
+    fprintf(stderr, "Open bracket did not follow closed parenthese, invalid format.\n");
+    exit(1);
+  }
+  //Create func body
+  funcNode->fields.children.right = parseStatement(tokens);
+  currToken = popToken(tokens);
+  if(currToken == NULL || currToken->type != CLOSED_BRACE){
+    fprintf(stderr, "Closed bracket missing for function %s, invalid format.\n", funcName);
+    exit(1);
+  }
+  return funcNode;
+}
+
+astnode_t* parseProgram(tokenlist_t *tokens){
+  printf("── parsing %s ──\n", sourcePath);
+  astnode_t *root = NULL;
+  if(tokens == NULL){
+    fprintf(stderr, "Cannot parse program, null token list.\n");
+    exit(1);
+  }
+  root = (astnode_t *) malloc(sizeof(astnode_t)*1);
+  if(root == NULL){
+    fprintf(stderr, "Failed to allocate space for root AST Node.\n");
+    exit(1);
+  }
+  root->nodeType = PROGRAM;
+  root->fields.children.left = parseFunction(tokens);
+  printf(" - parsing complete -\n\n", sourcePath);
+  return root;
+}
+
+void printASTNodeType(astnode_t *node){
+  if(node == NULL)
+    return;
+  AST_TYPE nodeType = node->nodeType;
+  switch(nodeType){
+    case PROGRAM:
+      printf("PROGRAM");
+      break;
+    case FUNCTION:
+      printf("FUNCTION");
+      break;
+    case STATEMENT:
+      printf("STATEMENT");
+      break;
+    case EXPRESSION:
+      printf("EXPRESSION");
+      break;
+    case FUNCNAME:
+      printf("FUNCNAME");
+      break;
+  }
+}
+
+void printAST(astnode_t *root){
+  if(root == NULL)
+    return;
+}
+
 int main(int argc, char *argv[]) {
   if(argc < 2){
-    fprintf(stderr, "Usage: %s <source code file>\n\nThis compiler should generate an object file, linkable with gcc for x86.\n", argv[0]);
+    fprintf(stderr, "Usage: %s <source code file>\n\n  This compiler should generate an assembly file, assemblable and linkable with:\n\tgcc <generated .s file> -m32 -o <output file> for x86.\n", argv[0]);
     exit(1);
   }
   strncpy(sourcePath, argv[1], LEN_PATH);
@@ -335,8 +510,11 @@ int main(int argc, char *argv[]) {
   initRegexp();
   tokenlist_t *tokens = lex();
   printf("Token List Size: %d\n", tokenListSize);
-  printTokens(tokens);
-
+  //printTokens(tokens);
+  puts("");
+  astnode_t * progAST = parseProgram(tokens);
+  printf("── printing AST ──\n");
+  printAST(progAST);
   //Free's
   freeTokens(tokens);
   freeRegs();
