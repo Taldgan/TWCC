791152153fe788b6d0f8276bb63e05ae802072e7
Taldgan <bananaphone993@live.com>
2022-06-15 19:40:07 -0700

    Fixed generated output file name

--- src/gen.c
+++ src/gen.c
@@ -4,6 +4,29 @@
 #include <stdlib.h>
 #include <string.h>
 
+
+FILE *getOutFile(){
+  int i;
+  int pathLen = strnlen(sourcePath, LEN_PATH)-1;
+  char outPath[LEN_PATH] = "";
+  //find first occurrence of '/' (working backwards)
+  for(i = pathLen; i > 0; i--){
+    if(sourcePath[i] == '/'){
+      i++;
+      break;
+    }
+  }
+  strncpy(outPath, &sourcePath[i], strnlen(&sourcePath[i], LEN_PATH)-1);
+  strncat(outPath, "s", 2);
+  printf("Output file: %s\n", outPath);
+  FILE *outFile = fopen(outPath, "w");
+  if(outFile == NULL){
+    fprintf(stderr, "Failed to open output file %s for writing.\n", outPath);
+    exit(1);
+  }
+  return outFile;
+}
+
 /**
  * generate(astnode_t *root)
  * Given a valid AST, generates assemblable assembly and writes it to a file.
@@ -16,16 +39,7 @@ void generate(astnode_t *root){
     fprintf(stderr, "Null AST node, cannot generate assembly.\n");
     exit(1);
   }
-  //After checking for non-null root node, open output file
-  char outPath[LEN_PATH] = "";
-  strncpy(outPath, sourcePath, strnlen(sourcePath, LEN_PATH)-1);
-  strncat(outPath, "s", 2);
-  printf("Output file: %s\n", outPath);
-  FILE *outFile = fopen(outPath, "w");
-  if(outFile == NULL){
-    fprintf(stderr, "Failed to open output file %s for writing.\n", outPath);
-    exit(1);
-  }
+  FILE *outFile = getOutFile();
   //Now traverse AST and use it to generate assembly
   astnode_t *currNode = root->fields.children.left;
   char *funcName = currNode->fields.children.left->fields.strVal;
